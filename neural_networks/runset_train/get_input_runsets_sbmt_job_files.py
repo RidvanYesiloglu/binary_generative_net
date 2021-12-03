9c7f7558f97f541fd79b175d456f2effe3034cab
RidvanYesiloglu <ridvan@stanford.edu>
2021-12-02 16:16:59 -0800

    Update get_input_runsets_sbmt_job_files.py

--- neural_networks/runset_train/get_input_runsets_sbmt_job_files.py
+++ neural_networks/runset_train/get_input_runsets_sbmt_job_files.py
@@ -3,7 +3,7 @@ import submit_job_files_given_opts
 import os
 def main():
     print('cwdd', os.getcwd())
-    dict_file = "../params_dictionary"
+    dict_file = "GNSS_Code_Design_Project/neural_networks/params_dictionary"
     opt_strs, params_dict = get_input_runsets_crt_opts.main(dict_file)
     print(opt_strs)
     submit_job_files_given_opts.main(opt_strs.split("\n"), params_dict)
