ca9d08ddd030889a4291917a676630c1cd23b038
RidvanYesiloglu <ridvan@stanford.edu>
2021-12-02 16:16:03 -0800

    getcwd

--- neural_networks/runset_train/get_input_runsets_sbmt_job_files.py
+++ neural_networks/runset_train/get_input_runsets_sbmt_job_files.py
@@ -1,7 +1,8 @@
 import get_input_runsets_crt_opts
 import submit_job_files_given_opts
-
+import os
 def main():
+    print('cwdd', os.getcwd())
     dict_file = "../params_dictionary"
     opt_strs, params_dict = get_input_runsets_crt_opts.main(dict_file)
     print(opt_strs)
