28d47924dc3c24769b998a78b1d0d4ce88562bbd
Ridvan Yesiloglu <92061399+RidvanYesiloglu@users.noreply.github.com>
2021-12-05 14:55:15 -0800

    Update and rename pull_main_and_runset_train.sh to clone_main_and_runset_train.sh

--- clone_main_and_runset_train.sh
+++ clone_main_and_runset_train.sh
@@ -1,4 +1,4 @@
 cd ..
-git pull git@github.com:RidvanYesiloglu/${proj_name}.git # pull
+git clone git@github.com:RidvanYesiloglu/${proj_name}.git # clone main
 chmod +x ./GNSS_Code_Design_Project/neural_networks/runset_train.sh
 ./GNSS_Code_Design_Project/neural_networks/runset_train.sh #run runset_train.sh bash file
