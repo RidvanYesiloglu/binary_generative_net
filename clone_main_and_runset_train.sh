386bab6dd3a8f202078300e298fd05dd7f695c06
Ridvan Yesiloglu <92061399+RidvanYesiloglu@users.noreply.github.com>
2021-12-05 15:33:48 -0800

    Update clone_main_and_runset_train.sh

--- clone_main_and_runset_train.sh
+++ clone_main_and_runset_train.sh
@@ -6,10 +6,13 @@ then
   eval "$(ssh-agent -s)" # start ssh-agent
   chmod 400 $ssh_key_file # give yourself read permission for the ssh-key
   ssh-add $ssh_key_file # add the key to authorize you for cloning
+  git init
+  git pull git@github.com:RidvanYesiloglu/${proj_name}.git #clone
+else
+# Tara's pull command with git pull https here.
 fi
 
-git init
-git pull git@github.com:RidvanYesiloglu/${proj_name}.git #clone
+
 cd ..
 chmod +x ./GNSS_Code_Design_Project/neural_networks/runset_train.sh
 ./GNSS_Code_Design_Project/neural_networks/runset_train.sh #run runset_train.sh bash fil
