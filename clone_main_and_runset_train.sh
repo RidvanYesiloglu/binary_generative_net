67ef33993511e755a44d2db9df0678acbcc3e69e
Ridvan Yesiloglu <92061399+RidvanYesiloglu@users.noreply.github.com>
2021-12-05 15:03:46 -0800

    Update clone_main_and_runset_train.sh

--- clone_main_and_runset_train.sh
+++ clone_main_and_runset_train.sh
@@ -1,4 +1,4 @@
 cd ..
-git clone https://github.com/RidvanYesiloglu/GNSS_Code_Design_Project.git #git@github.com:RidvanYesiloglu/${proj_name}.git # clone main
+git pull https://github.com/RidvanYesiloglu/GNSS_Code_Design_Project.git #git@github.com:RidvanYesiloglu/${proj_name}.git # clone main
 chmod +x ./GNSS_Code_Design_Project/neural_networks/runset_train.sh
 ./GNSS_Code_Design_Project/neural_networks/runset_train.sh #run runset_train.sh bash file
