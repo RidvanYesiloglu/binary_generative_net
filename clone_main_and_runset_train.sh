15b8f2434c8a244ccdca3ae07a6ed6b606b9966b
Ridvan Yesiloglu <92061399+RidvanYesiloglu@users.noreply.github.com>
2021-12-05 15:25:15 -0800

    Update clone_main_and_runset_train.sh

--- clone_main_and_runset_train.sh
+++ clone_main_and_runset_train.sh
@@ -1,4 +1,12 @@
 proj_name="GNSS_Code_Design_Project"
+read -p "Do you want ssh permit (yes:1, no:0)? : " ssh_request
+if $ssh_request==1 ; then
+ssh_key_file="git_ssh_keys/id_ed25519"
+eval "$(ssh-agent -s)" # start ssh-agent
+chmod 400 $ssh_key_file # give yourself read permission for the ssh-key
+ssh-add $ssh_key_file # add the key to authorize you for cloning
+fi
+
 git init
 git pull git@github.com:RidvanYesiloglu/${proj_name}.git #clone
 cd ..
