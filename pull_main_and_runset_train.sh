f5510b9f032bd66fb17b6f42d6e855cc63b4a3f8
Ridvan Yesiloglu <92061399+RidvanYesiloglu@users.noreply.github.com>
2021-12-05 16:13:52 -0800

    Update pull_main_and_runset_train.sh

--- pull_main_and_runset_train.sh
+++ pull_main_and_runset_train.sh
@@ -5,6 +5,7 @@ git rm .gitattributes
 git add -A
 git reset --hard
 chmod +x pull_main_and_runset_train.sh #make this runnable after reset
+echo "Now, main branch will be pulled."
 read -p "Do you want Tara pull or Ridvan pull (tara:T, ridvan:R)? : " ssh_request
 if (( ${ssh_request} == R )); then
   ssh_key_file="../git_ssh_keys/id_ed25519"
