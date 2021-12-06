c8f7a9296c14a0b93cb3e965a7ab12291d596220
Ridvan Yesiloglu <92061399+RidvanYesiloglu@users.noreply.github.com>
2021-12-05 16:00:10 -0800

    Update pull_main_and_runset_train.sh

--- pull_main_and_runset_train.sh
+++ pull_main_and_runset_train.sh
@@ -1,8 +1,7 @@
 proj_name="GNSS_Code_Design_Project"
 git init
-#git reset --hard # ignore sherlock changes
-git stash
-git stash pop
+git reset --hard # ignore sherlock changes
+git clean -xdf
 chmod +x pull_main_and_runset_train.sh #make this runnable after reset
 read -p "Do you want Tara pull or Ridvan pull (tara:T, ridvan:R)? : " ssh_request
 if (( ${ssh_request} == R )); then
