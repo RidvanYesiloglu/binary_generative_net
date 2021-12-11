8a9512c5d477201cb745732f759156160b5dee1c
Ridvan Yesiloglu <92061399+RidvanYesiloglu@users.noreply.github.com>
2021-12-11 22:38:45 +0300

    Update remove_from_waitings.py

--- neural_networks/runset_train/remove_from_waitings.py
+++ neural_networks/runset_train/remove_from_waitings.py
@@ -1,8 +1,8 @@
 import sys
-import parameters
+from runset_train import parameters
 import os
 
-params_dict = parameters.decode_arguments_dictionary("/home/groups/gracegao/Low_Corr_Bin_Code_Design/params_dictionary")
+params_dict = parameters.decode_arguments_dictionary("params_dictionary")
 args = parameters.get_arguments(params_dict)
 repr_str = parameters.create_repr_str(args, [info.name for info in params_dict.param_infos])
 waitings_folder = os.path.join("/scratch/groups/gracegao/Low_Corr_Bin_Code_Design/waiting_jobs", repr_str)
