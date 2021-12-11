13e9ea44d543a4607cf1e9a8f70d06f3f769b88e
Ridvan Yesiloglu <92061399+RidvanYesiloglu@users.noreply.github.com>
2021-12-11 13:33:26 +0300

    Update submit_exps.sh

--- neural_networks/submit_exps.sh
+++ neural_networks/submit_exps.sh
@@ -1,2 +1,2 @@
-cd ..
-python3 ./GNSS_Code_Design_Project/neural_networks/runset_train/get_input_runsets_sbmt_job_files.py
+cd ../..
+python3 ./gnss_code_design/neural_networks/runset_train/get_input_runsets_sbmt_job_files.py
