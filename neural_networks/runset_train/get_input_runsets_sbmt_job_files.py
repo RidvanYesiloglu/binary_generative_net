fa66da7ace00adcc7000fc914c1417cefeea7780
Ridvan Yesiloglu <92061399+RidvanYesiloglu@users.noreply.github.com>
2021-12-11 13:40:03 +0300

    Update get_input_runsets_sbmt_job_files.py

--- neural_networks/runset_train/get_input_runsets_sbmt_job_files.py
+++ neural_networks/runset_train/get_input_runsets_sbmt_job_files.py
@@ -2,7 +2,6 @@ import get_input_runsets_crt_opts
 import submit_job_files_given_opts
 import os
 def main():
-    print('cwdd', os.getcwd())
     dict_file = "gnss_code_design/neural_networks/params_dictionary"
     opt_strs, params_dict = get_input_runsets_crt_opts.main(dict_file)
     print(opt_strs)
