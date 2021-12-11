88e515e1c752243d8ae5736269cabff99af28572
Ridvan Yesiloglu <92061399+RidvanYesiloglu@users.noreply.github.com>
2021-12-11 21:50:44 +0300

    Update get_input_runsets_sbmt_job_files.py

--- neural_networks/runset_train/get_input_runsets_sbmt_job_files.py
+++ neural_networks/runset_train/get_input_runsets_sbmt_job_files.py
@@ -4,11 +4,11 @@ import os
 def main():
     dict_file = "params_dictionary"
     print("1 burasi")
-"""    opt_strs, params_dict = get_input_runsets_crt_opts.main(dict_file)
+    opt_strs, params_dict = get_input_runsets_crt_opts.main(dict_file)
     print("2 burasi")
     print(opt_strs)
     submit_job_files_given_opts.main(opt_strs.split("\n"), params_dict)
-    print("3 burasi")"""
+    print("3 burasi
 
 if __name__ == "__main__":
     main()
