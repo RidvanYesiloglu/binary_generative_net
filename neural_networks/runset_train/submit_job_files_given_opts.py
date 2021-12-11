cf17dd95c24adef1dce0d92ae94fd5908c9d349d
Ridvan Yesiloglu <92061399+RidvanYesiloglu@users.noreply.github.com>
2021-12-11 18:18:14 +0300

    Update submit_job_files_given_opts.py

--- neural_networks/runset_train/submit_job_files_given_opts.py
+++ neural_networks/runset_train/submit_job_files_given_opts.py
@@ -40,43 +40,36 @@ def main(opts_strs, params_dict):
                 curr_opts = get_input_runsets_crt_opts.create_opts_strs([args], params_dict).split("\n")[0]
                 run_commands.append("python3 {}train.py{}\n".format(code_dir, curr_opts))
                 curr_N *= 2
-        #print(run_commands)
+                
         script = ""
         script += "#!/bin/bash\n"
         script += "#SBATCH --time={}:00:00\n".format(job_time)
         script += "#SBATCH --job-name={}\n".format(repr_str)
-        #script += "#SBATCH --mail-user=ridvan@stanford.edu\n"
-        #script += "#SBATCH --mail-type=BEGIN,END\n" # mail on beginning and end
+        script += "#SBATCH --mail-user=ridvan@stanford.edu\n"
+        script += "#SBATCH --mail-type=BEGIN,END\n" # mail on beginning and end
         
         script +=  "#SBATCH --output={}%j.%x.out\n".format(slurm_submit_dir)
         script +=  "#SBATCH --error={}%j.%x.err\n".format(slurm_submit_dir)
-        #script += "#SBATCH --output=solution.OUT\n"
-        #script += "#SBATCH --error=FAILURE.e%j\n"
-         
+
         script += "#SBATCH --nodes=1\n"
         script += "#SBATCH --ntasks-per-node=1\n"
         script += "#SBATCH --partition=gpu\n"
         script += "#SBATCH --gpus=1\n"
         #script += "#SBATCH -C GPU_MEM:32GB" if needed
+        
         script += "module load python/3.9.0\n"
         script += "module load py-numpy/1.20.3_py39\n"
         script += "module load py-pytorch/1.8.1_py39\n"
         script += "module load py-matplotlib/3.4.2_py39\n"
         script += "module load cuda/11.2.0\n"
-       # script += "pwd\n"
-        script += "export SLURM_SUBMIT_DIR={}\n".format(slurm_submit_dir)
-       # script += 'cd $SLURM_SUBMIT_DIR\n'
-        script += "python3 {}remove_from_waitings.py{}\n".format(os.path.join(code_dir,'runset_train'), opts)
-       # script += 'cd ..\n'
+
+        script += "python3 {}/remove_from_waitings.py{}\n".format(os.path.join(code_dir,'runset_train'), opts)
         #script += 'nvidia-smi' if needed
         for command in run_commands:
-            script += command
-
-        
+            script += command 
         job_script_file = open(job_script_name, "w+")
         job_script_file.write(script)
         job_script_file.close()
-       # rc = call("cd {}".format(slurm_submit_dir), shell=True)
         rc = call("sbatch {}".format(job_script_name), shell=True)
 if __name__ == "__main__":
     main()
