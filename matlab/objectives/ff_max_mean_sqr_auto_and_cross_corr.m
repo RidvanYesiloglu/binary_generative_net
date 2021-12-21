36471aa81ee72b543cff782f7f2b379db314835d
taraymina <71675541+tymina95@users.noreply.github.com>
2021-12-20 22:52:18 -0600

    Removed popsize as global variable; fixed minor bug with calling calculate auto/cross correlation (setting to always normalize)

--- matlab/objectives/ff_max_mean_sqr_auto_and_cross_corr.m
+++ matlab/objectives/ff_max_mean_sqr_auto_and_cross_corr.m
@@ -1,17 +1,18 @@
 function [max_cost, auto_obj, cross_obj] = ff_max_mean_sqr_auto_and_cross_corr(pop)
 
     % x is complete population 
-    global popsize
     global nbits
     global npar
     global start_indices
     global pairs
     global npairs
     
-    auto_comp = calc_autocorr(pop, npar, popsize, nbits, start_indices);
+    popsize = size(pop,1);
+    
+    auto_comp = calc_autocorr(pop, npar, popsize, nbits, start_indices,1);
     % Remove first row to zero to ignore zero-lag autocorrelation
     auto_comp = auto_comp(:, :, 2:nbits);
-    cross_comp = calc_crosscorr(pop, popsize, nbits, pairs, npairs);
+    cross_comp = calc_crosscorr(pop, popsize, nbits, pairs, npairs,1);
 
     % Include the mean auto- and cross-correlation cost components
     % And normalize the correlation (divide by nbits again)
